19f7b821903cf1f70c273fdc9e5ecd0392897249
James Killelea <dylan.killelea@yahoo.com>
2022-01-22 12:06:45 -0600

    restructured my objects to upscale versatility

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,74 @@
+var seconds = 60;
+let questionNumber = 0;
 let questions = [
     {
-        question: "Inside which HTML element do we put the JavaScript?",
-        choices: ["function myFunction()", "myFunction", "function = myFunction()", "makeAFunctionPlease"],
-        answer: "function MyFunction()"
+        key:1,
+        question: "What is the correct way to start a function?",
+        choices: [
+            {
+                text: "function myFunction()",
+                answer: true
+            },
+            {
+                text: "myFunction",
+                answer: false
+            },
+            {
+                text: "function = myFunction()",
+                answer: false
+            },
+            {
+                text: "makeAFunctionPlease",
+                answer: false
+            }
+        ],
+        
     },
     {
+        key: 2,
         question: "How do you call a function named 'pizza'?",
-        choices: ["eatPizza", "eatMyPizza", "call pizza()", "pizza()"],
-        answer: "pizza()"
-    }
+        choices: [
+            {
+                text: "eatPizza",
+                answer: false
+            },
+            {
+                text: "eatMyPizza",
+                answer: false
+            },
+            {
+                text:"call pizza()",
+                answer: false
+            },
+            {
+                text:"pizza()",
+                answer: true
+            }
+        ],
+        
+    },
+    {
+        key:3,
+        question: "Where is the correct place on an HTML element to insert your Javascript",
+        choices: [
+            {
+                text: "The top of the CSS sheet",
+                answer: false
+            },
+            {
+                text: "Top of the 'body' element",
+                answer: false
+            },
+            {
+                text: "Bottom of the 'body' element",
+                answer: true
+            },
+            {
+                text:"In the 'head' tag",
+                answer: false
+            }      
+        ],  
+    },
 ]
 //hides start page then shows the question div
 //starts timer 
@@ -18,7 +78,6 @@ document.querySelector("#start-btn").addEventListener("click",function(){
     document.querySelector(".start-page").style.display = "none"
     document.querySelector("#question-page").style.display = "block"
     function countdown(minutes) {
-        var seconds = 60;
         var mins = minutes
         function tick() {
             var timeLeft = document.getElementById("time-left");
@@ -40,6 +99,7 @@ document.querySelector("#start-btn").addEventListener("click",function(){
     }
     
     countdown(1);
+    displayQuestion();
 })
 // document.getElementById("answer-choice1").innerHTML = "1. function myFunction()";
 // document.getElementById("answer-choice2").innerHTML = "2. myFunction()";
@@ -59,3 +119,93 @@ document.querySelector("#start-btn").addEventListener("click",function(){
 //scores shoould be able to be seen
 //after game to presented with score
 //after game be able to input initials
+function displayQuestion() {
+    let currentQuestion = questions[questionNumber];
+    console.log("current question: ",currentQuestion.question);
+    let q1 = document.getElementById("question");
+    console.log(q1);
+    q1.innerText = currentQuestion.question;
+    let choicesList= document.getElementById("choices");
+
+    currentQuestion.choices.map(choice=> {
+        let element = document.createElement('li');
+        console.log(choice.text);
+        element.innerText = choice.text
+        //change to innerHTML to insert a button
+        //insert your choice text and choice answer
+        
+        choicesList.appendChild(element) })
+
+    // {"key": 1,
+    // "question": "What is the correct way to start a function?",
+    // "choices": [
+    //     {
+    //         "text": "function myFunction()",
+    //         "answer": true
+    //     },
+    //     {
+    //         "text": "myFunction",
+    //         "answer": false
+    //     },
+    //     {
+    //         "text": "function = myFunction()",
+    //         "answer": false
+    //     },
+    //     {
+    //         "text": "makeAFunctionPlease",
+    //         "answer": false
+    //     }
+    // ]
+// }
+    questionNumber++;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     document.querySelector(".question").innerHTML = currentQuestion.question;
+//     document.getElementById("answer-choice1").innerHTML = currentQuestion.choices[0]
+//     document.getElementById("answer-choice2").innerHTML = currentQuestion.choices[1]
+//     document.getElementById("answer-choice3").innerHTML = currentQuestion.choices[2]
+//     document.getElementById("answer-choice4").innerHTML = currentQuestion.choices[3]
+//     var answer1 = document.getElementById("answer-choice1").addEventListener("click", function(){
+//         console.log("hey there");
+//         // if (currentQuestion.choices = currentQuestion.answer){
+//         //     console.log("correct");
+//         // }
+//         // else {
+//         //     console.log("incorrect");
+//         // }
+//         questionNumber++;
+//         displayQuestion();
+//     })
+//     var answer1 = document.getElementById("answer-choice2").addEventListener("click", function(){
+//         questionNumber++;
+//         displayQuestion();
+//     })
+//     var answer1 = document.getElementById("answer-choice3").addEventListener("click", function(){
+//         questionNumber++;
+//         displayQuestion();
+//     })
+//     var answer1 = document.getElementById("answer-choice4").addEventListener("click", function(){
+//         questionNumber++;
+//         displayQuestion();
+//     })
+//     if (answer1 == currentQuestion.answer){
+//         console.log("correct");
+//     }
+//     else {
+//         console.log("incorrect");
+//     }
+// }
